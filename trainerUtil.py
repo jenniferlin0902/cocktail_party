fde342adf9d153c9850e545f34c15bd0926b1a1a
Jennifer <jennlinc@stanford.edu>
2017-11-08 22:26:17 -0800

    add cocktailData class to trainerUtil

--- trainerUtil.py
+++ trainerUtil.py
@@ -7,6 +7,10 @@ Utilities for use in developing cocktail recipe generator
 @author: Suzannah
 """
 import re
+import random
+import os
+
+TOTAL_DATA_SIZE = 5568
 
 def retrieveData(inputFile, useTitles = 0, simplifyIngredients = 1, removeBranding = 1):
     """
@@ -33,7 +37,7 @@ def retrieveData(inputFile, useTitles = 0, simplifyIngredients = 1, removeBrandi
             recipe['TITLE'] = title
         # Recipes are scraped with semicolons separating ingredients
         ingredients = text.split(';')
-        for j in range (0,len(ingredients)):
+        for j in range(0,len(ingredients)):
             # The list may shrink as it's corrected. This ensures we don't 
             # overrun the list length.
             if j > len(ingredients) - 1: break
@@ -57,7 +61,99 @@ def retrieveData(inputFile, useTitles = 0, simplifyIngredients = 1, removeBrandi
                 ingredientwords.pop(0)
             except ValueError:
                 amt = 1
-            key = ' '.join(ingredientwords)
-            recipe[key] = amt
+            key = ' '.join(ingredientwords[1:]).lower().rstrip()
+            recipe[key] = (amt, ingredientwords[0])# ingredient : (amt, unit)
         recipes.append(recipe)
     return recipes
+
+def split_data():
+    '''
+    helper function to split up recipe text file
+    '''
+    n_classifier = 1000
+    all = range(TOTAL_DATA_SIZE)
+    random.shuffle(all)
+    classifier = all[:n_classifier]
+    count = 0
+    test_out = open("cocktail_test.txt", "w")
+    train_out = open("cocktail_train.txt", "w")
+
+    with open("cocktail_all.txt") as f:
+        for line in f:
+            if count in classifier:
+                test_out.write(line)
+            else:
+                train_out.write(line)
+            count += 1
+
+
+def vector_to_recipe(vector, ingredient_list):
+    '''
+    helper function to convert an ingredient vector to a list of ingredient
+    '''
+    recipe = []
+    for i, a in enumerate(vector):
+        if a > 0:
+            recipe.append(ingredient_list[i])
+    return recipe
+
+
+class cocktailData:
+    '''
+    cocktail recipe data class
+
+    @param
+    self.n_recipe - number of recipes
+    self.raw_data - list of recipe dictionary {[ingredient name]:[qty]}
+    self.n_ingredient - total number of distinct ingredient
+    self.ingredients - {ingredient: (frequency, set(unit)}
+    self.recipes - list of recipe ingredient vector
+    '''
+
+    def __init__(self, file):
+        if not os.path.exists(file):
+            print "ERROR: training data file {} does not exist".format(file)
+        else:
+            self.raw_data = retrieveData(file, simplifyIngredients=1, removeBranding=1)
+            self.ingredients = {}
+            for recipe in self.raw_data:
+                for k, v in recipe.iteritems():
+                    if k not in self.ingredients:
+                        self.ingredients[k] = [0, set([v[1]])] #(count, unit list)
+                    else:
+                        # otherwise, increment count, and keep track of unit
+                        self.ingredients[k][0] += 1
+                        self.ingredients[k][1].add(v[1])
+            self.n_recipe = len(self.raw_data)
+            self.n_ingredient = len(self.ingredients.keys())
+        self.recipes = []
+        for recipe in self.raw_data:
+            recipe_vector = [0] * self.n_ingredient
+            for l in recipe:
+                recipe_vector[self.ingredients.keys().index(l)] = 1
+            self.recipes.append((recipe_vector, 1))
+        self.size = len(self.recipes)
+
+    def get_ingredient_list(self):
+        # return a copy in case someone modify this
+        return list(self.ingredients.keys())
+
+    def get_recipe_raw(self, id):
+        return self.raw_data[id]
+
+    def get_recipes_binary(self):
+        return list(self.recipes)
+
+    def generate_fake(self, n):
+        num_ingredients = [3,4,5,6]
+        fakes = []
+        for _ in range(n):
+            n_ingredient = random.choice(num_ingredients)
+            recipe_vector = [0] * self.n_ingredient
+            for i in range(n_ingredient):
+                recipe_vector[random.choice(range(self.n_ingredient))] = 1
+            fakes.append((recipe_vector, 0))
+        return fakes
+
+    def get_ingredient_unit(self, ingredient):
+        return self.ingredients[ingredient][2]
